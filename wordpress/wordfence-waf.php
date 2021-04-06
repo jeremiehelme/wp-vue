726fdb0e8a952259b147b3429a48139610410421
jérémie helme <jeremiehelme@gmail.com>
2021-04-06 19:14:32 +0200

    adding base plugins

---
+++ wordpress/wordfence-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists('/Users/jeremiehelme/Documents/Dave/Web/wp-vue/wordpress/wp-content/plugins/wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", '/Users/jeremiehelme/Documents/Dave/Web/wp-vue/wordpress/wp-content/wflogs/');
+	include_once '/Users/jeremiehelme/Documents/Dave/Web/wp-vue/wordpress/wp-content/plugins/wordfence/waf/bootstrap.php';
+}
+?>
